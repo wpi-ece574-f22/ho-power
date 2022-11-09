98b2782b8ac138c311bbe0ba6aed9aff763804e9
Patrick Schaumont <patrickschaumont@users.noreply.github.com>
2022-11-09 16:58:09 -0500

    Update sta.hd.cmd

--- bcounter32/syn/sta.hd.cmd
+++ bcounter32/syn/sta.hd.cmd
@@ -1,6 +1,6 @@
 read_liberty /mnt/volume_nyc1_01/skywater-pdk/libraries/sky130_fd_sc_hd/latest/timing/sky130_fd_sc_hd__ff_n40C_1v76.lib
-read_verilog bcounter_gl.v
-link_design bcounter
+read_verilog bcounter32_gl.v
+link_design bcounter32
 
 create_clock -name clk -period 10 {clk}
 set_input_delay -clock clk 0 {reset}
